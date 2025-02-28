e10017ac9c2a8985a612b5eca9ca18c5c53eb281
Ученик <Vika1809dok@rambler.ru>
2025-02-28 18:07:38 +0300

    css

--- 1.py
+++ 1.py
@@ -70,5 +70,39 @@ def return_sample_page1():
                   </body>
                 </html>"""
 
+
+@app.route('/choice/<planet_name>')
+def sample_file_upload(planet_name):
+    return f'''<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <title>Привет, Марс!</title>
+                    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+                    <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                  </head>
+                  <body>
+                  <h1>Мое предложение: {planet_name}</h1>
+                  <h2> Эта планета</h2>
+                    <div class="alert alert-success" role="alert">
+                    <p>Человечество вырастает из детства.</p>
+                    </div>
+                    <div class="alert alert-primary" role="alert">
+                        Человечеству мала одна планета.
+                    </div>
+                    <div class="alert alert-light" role="alert">
+                    Мы сделаем обитаемыми безжизненные пока планеты.
+                    </div>
+                    <div class="alert alert-primary" role="alert">
+                        И начнем с {planet_name}!
+                    </div>
+                    <div class="alert alert-dark" role="alert">
+                      Присоединяйся!
+                    </div>
+                    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+                  </body>
+                </html>'''
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
