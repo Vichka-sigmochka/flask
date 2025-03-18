4d661741ca4ce92ec72765975a8755920047374f
Ученик <Vika1809dok@rambler.ru>
2025-03-18 18:01:46 +0300

    3

--- flask1/data/users.py
+++ flask1/data/users.py
@@ -10,7 +10,7 @@ class User(SqlAlchemyBase):
                            primary_key=True, autoincrement=True)
     surname = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    aga = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+    age = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
     position = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     speciality = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     address = sqlalchemy.Column(sqlalchemy.String, nullable=True)
--- flask1/main.py
+++ flask1/main.py
@@ -8,7 +8,7 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
 def main():
-    db_session.global_init("db/mars_explorer.db")
+    db_session.global_init("db/mars_explorer1.db")
     user = User()
     user.surname = "Scott"
     user.name = "Ridley"
@@ -19,6 +19,24 @@ def main():
     user.email = "scott_chief@mars.org"
     db_sess = db_session.create_session()
     db_sess.add(user)
+    user1 = User()
+    user1.surname = "Biba"
+    user1.name = "Boba"
+    user1.age = 10
+    user1.position = 'doctor'
+    user1.speciality = "enginner"
+    user1.address = 'module'
+    user1.email = "bibaboba@mars.org"
+    db_sess.add(user1)
+    user2 = User()
+    user2.surname = "Pomidor"
+    user2.name = "Byba"
+    user2.age = 40
+    user2.position = 'listener'
+    user2.speciality = "cooking"
+    user2.address = 'module_2'
+    user2.email = "pomidorpomidor@mars.org"
+    db_sess.add(user2)
     db_sess.commit()
     #app.run()
 
