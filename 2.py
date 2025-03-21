8388fde479c57733e9613fc5a961b8187562828d
Ученик <Vika1809dok@rambler.ru>
2025-03-21 17:21:18 +0300

    двойная защита

--- 2.py
+++ 2.py
@@ -1,8 +1,7 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, redirect
+from loginform import LoginForm
 app = Flask(__name__)
-
-
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 @app.route('/<word>')
 @app.route('/index/<word>')
@@ -16,6 +15,13 @@ def pr(prof):
 @app.route('/list_prof/<list>')
 def ls(list):
     return render_template('list_prof.html', param=list)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('login.html', title='Авторизация', form=form)
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
